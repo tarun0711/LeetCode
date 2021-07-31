ec011694f371d5c2388cc329c913910e916653c6
Tarun Kumar <68737903+tarun0711@users.noreply.github.com>
2021-07-31 10:09:55 +0530

    Create HalfArray.java

---
+++ July Leetcode Challenge/HalfArray.java
@@ -0,0 +1,23 @@
+class HalfArray {
+    public int minSetSize(int[] arr) 
+    {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i =0;i<arr.length;i++)
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        
+        List<Integer> lst= new ArrayList<>(map.values());
+        Collections.sort(lst,Collections.reverseOrder());
+        
+        int i=0,c=0,total=0;
+        
+        while(total<arr.length/2)
+        {
+            total+=lst.get(i);
+            i++;
+            c++;
+        }
+        
+        return c;
+    }
+}
