058754d8e74e96a854beaff866bc0f572084c9ad
Tarun Kumar <68737903+tathaastu-tarun@users.noreply.github.com>
2021-07-31 16:01:19 +0530

    Update class Ksmallest.java

--- July Leetcode Challenge/class Ksmallest.java
+++ July Leetcode Challenge/class Ksmallest.java
@@ -1,18 +1,54 @@
 import java.util.*;
 class Ksmallest {
+    //O(n^2) approach
+//     public int kthSmallest(int[][] matrix, int k)
+//     {
+        
+//         ArrayList<Integer> lst = new ArrayList<>();
+//         for(int i=0;i<matrix.length;i++)
+//         {
+//             for(int j=0;j<matrix[0].length;j++)
+//             {
+//                 lst.add(matrix[i][j]);
+//             }
+//         }
+//         Collections.sort(lst);
+//         return lst.get(k-1);
+        
+//    }
+    //O(log(n)) approach
+    
     public int kthSmallest(int[][] matrix, int k)
     {
+        int low = matrix[0][0], high = matrix[matrix.length-1][matrix.length-1];
         
-        ArrayList<Integer> lst = new ArrayList<>();
-        for(int i=0;i<matrix.length;i++)
+        while(low<high)
         {
-            for(int j=0;j<matrix[0].length;j++)
+            int mid = low+(high-low)/2;
+            int rank = calRank(matrix,mid);
+            if(rank<k)
+                low= mid+1;
+            else
+              high=mid;
+        }
+        return low;
+    }
+    
+    public int calRank(int [][]mat, int r)
+    {
+        int j=0,i=mat.length-1,count=0;
+        
+        while(i>=0 && j<mat.length)
+        {
+            if(mat[i][j]>r) i--;
+            
+            else
             {
-                lst.add(matrix[i][j]);
+                count= count+i+1;
+                j++;
             }
         }
-        Collections.sort(lst);
-        return lst.get(k-1);
-        
+        return count;
     }
+    
 }
